1f0b1dc348665c25269e4fe246c31f347b44e25f
Vansh Aggarwal <vansh.aggarwal2@incedoinc.com>
2023-02-16 16:37:43 +0530

    cards css updated

---
+++ Frontend/Main/STAR_frontend/src/Components/Card/StatusCard1.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+export default function MediaControlCard1() {
+  const theme = useTheme();
+
+  return (
+    <Card sx={{ display: 'inline-block' , width: '17rem',marginLeft:'4.5rem'}} >
+      <Box sx={{ display: 'flex', flexDirection: 'column', float:'left' , width:'70%' }}>
+        <CardContent sx={{ }}>
+        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB00lEQVR4nO2WzTIDQRDHR8kT+Er3xlkV8XXz8Rg4IE+BJ+ARxNXn0UU8Qap2OpQjJ1WUO6K47vaoUS1B2E12k2zkwL+qLzPb+5vu6Zlppf7VQLaoUj45c4YwZwjWKoY5GZM5lbQ8nZliwkPW8GQIbZixhjITHnhnQxNtA20RBypAfK0HDC4AX2UB9sLpbw3qOiOs4TouMGQBt55OZ5uCejqdZY0vrUJr4C/WhbF4kZ4N97GGm3ahn3DCO+vCYCSYCY+SgtZEvt84xW5msplCaqbgPHKmG0V7mDQ0MmpbVCk5i50DQzn0kvFLznynoB/m66HZANhoXA1Jzz1r3DQaCj/GfdawXTH0v/vBifgw4UMAXnKWQ8CwEZKehfdtsKrHaLyqGd/+qgvI10Av5dvq+GIwaliPByZYjAbjTii4BEvxwC6uBMH4wIRbRsNpINUEeYEyIX/z01AQn9ip9rtVXPZY9TLB468fJ5E8Zx0E76l6kkecCU3iUELjldLjdcGdipoJdlVXnsUiDkSC3+EujCXSCBA823MYjQXtauvzIWnY5DlrqtkjNFLBsmWqXXlS7bKABudc5gQYWb2tSC4ZuX0q12u1oXdxxXfTMzLX0k/VX9EbsmilXotITyYAAAAASUVORK5CYII="/>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            Pending Request
+          </Typography>
+        </CardContent>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column',fontSize:'2rem',color:'blue',float:'left', width:'20% ', marginTop:'1.5rem',marginLeft:'0.5rem', textAlign:'center'}}>0 </Box>
+    </Card>
+  );
+}
--- Frontend/Main/STAR_frontend/src/Components/TabComponent/Tabs.js
+++ Frontend/Main/STAR_frontend/src/Components/TabComponent/Tabs.js
@@ -5,10 +5,12 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import './tabs.css';
-import StatusCard from '../Card/StatusCard'
+import StatusCard from '../Card/StatusCard';
 import UserInfo from './UserInfo';
 import Card from './OutlinedCard';
 import BasicTable from '../Userstatus/Userstatus';
+import StatusCard1 from '../Card/StatusCard1';
+import StatusCard2 from '../Card/StatusCard2';
 
 export default function Tabs() {
   const [value, setValue] = React.useState('1');
@@ -26,8 +28,8 @@ export default function Tabs() {
         </TabPanel>
         <TabPanel value="2" class = "status" style={{height: "9rem", overflowX: 'hidden'}}>         
           <StatusCard/>
-          <StatusCard/>
-          <StatusCard/>
+          <StatusCard1/>
+          <StatusCard2/>
         </TabPanel>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
