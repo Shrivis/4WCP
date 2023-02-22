d4156e54c3a8b9002556589c3acf85947df4e879
Vishal Srivastav <vishal.srivastav@incedoinc.com>
2023-02-22 16:37:47 +0530

    added email service for frontend

--- Frontend/Main/STAR_frontend/src/App.js
+++ Frontend/Main/STAR_frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter  as Router,Routes,Route,Link, BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route,Link, BrowserRouter} from 'react-router-dom';
 import Login from './Components/Login/login';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import DashboardComp from './Dashboard';
