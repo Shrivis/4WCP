8f3a2dd60245c34fbc6402be6e24afec91506b33
Rishu Rana <rishu.rana1@incedoinc.com>
2023-04-03 22:26:21 +0530

    optimised and commented dashboard

--- Frontend/Main/STAR_frontend/src/Components/TabComponent/Dashboard/Graph/Pie.js
+++ Frontend/Main/STAR_frontend/src/Components/TabComponent/Dashboard/Graph/Pie.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import ReactApexCharts from 'react-apexcharts'
+import ReactApexCharts from 'react-apexcharts';
 
-export default function Pie({horizontals, vals, ChartType}) {
-    const options = {
-      chart: {
-        width: '100%',
-      },
-      labels: horizontals,
-      legend:{position: 'left'},
-      dataLabels: {
-        enabled: false
-      },
-      responsive: [{
+export default function Pie({ horizontals, vals, ChartType }) {
+  // Options for the chart component
+  const options = {
+    chart: {
+      width: '100%', // Set chart width to 100%
+    },
+    labels: horizontals, // Horizontal axis labels for chart data
+    legend: { position: 'left' }, // Set legend position to left of chart
+    dataLabels: {
+      enabled: false, // Disable data labels in the chart
+    },
+    responsive: [
+      {
         breakpoint: 480,
         options: {
           chart: {
-            width: 240
+            width: 240, // Set chart width to 240px at the specified breakpoint
           },
-          
-        }
-      }],
-
-      fill: {
-        opacity: 0.8
+        },
       },
-      colors:['#1C4E80', '#DBAE58', '#202020'],
-    };
-    return (
-        <div id="chart">
-        <ReactApexCharts options={options} series={vals} type={ChartType} width={310} />
-        </div> 
-    );
+    ],
+    fill: {
+      opacity: 0.8, // Set opacity for chart fill
+    },
+    colors: ['#1C4E80', '#DBAE58', '#202020'], // Array of color values for the chart series
+  };
+
+  // Render a div containing the ApexChart component with provided props and options
+  return (
+    <div id="chart">
+      <ReactApexCharts options={options} series={vals} type={ChartType} width={310} />
+    </div>
+  );
 }
--- Frontend/Main/STAR_frontend/src/Components/TabComponent/Dashboard/Graph/ProjectBar.js
+++ Frontend/Main/STAR_frontend/src/Components/TabComponent/Dashboard/Graph/ProjectBar.js
@@ -1,67 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import ReactApexCharts from 'react-apexcharts';
-import {Spin} from 'antd';
+import { Spin } from 'antd';
 
-export default function ManagerBar({projectData}) {
-    const [projectName, setProjectName] = useState([]);
-    const [extraHour, setExtraHour] = useState([]);
-    const [underUtil, setUnderUtil] = useState([]);
-    const [loading, setLoading] = useState(true);
+export default function ManagerBar({ projectData }) {
+  const [projectName, setProjectName] = useState([]); // State variable to hold the names of the projects
+  const [extraHour, setExtraHour] = useState([]); // State variable to hold the extra hours for each project
+  const [underUtil, setUnderUtil] = useState([]); // State variable to hold the underutilized hours for each project
+  const [loading, setLoading] = useState(true); // State variable to indicate if data is still being loaded
 
-    useEffect(() => {
-        setProjectName(Object.keys(projectData));
-        setExtraHour(Object.entries(projectData).map(([key, value]) => value['first']));
-        setUnderUtil(Object.entries(projectData).map(([key, value]) => value['second']));
-        setLoading(false);
-    }, [] );
+  useEffect(() => {
+    const dataEntries = Object.entries(projectData); // Get an array of key-value pairs from projectData object
+    setProjectName(dataEntries.map(([key]) => key)); // Extract the keys (project names) and set them in state
+    setExtraHour(dataEntries.map(([, value]) => value.first)); // Extract the first values (extra hours) and set them in state
+    setUnderUtil(dataEntries.map(([, value]) => value.second)); // Extract the second values (underutilized hours) and set them in state
+    setLoading(false); // Set loading to false once the data is processed and stored in state
+  }, [projectData]); // Run this effect whenever projectData prop changes
 
-    const series = [{
-        name: 'Extra Hours Claimed',
-        data: extraHour,
-      }, {
-        name: 'Under Utilized Hours',
-        data: underUtil,
+  // Define the series and options objects for ApexCharts
+  const series = [
+    {
+      name: 'Extra Hours Claimed',
+      data: extraHour,
+    },
+    {
+      name: 'Under Utilized Hours',
+      data: underUtil,
+    },
+  ];
+  const options = {
+    chart: {
+      type: 'bar',
+      width: '100%',
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 1,
+        horizontal: true,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: projectName,
+    },
+    colors: ['#1C4E80', '#DBAE58', '#202020'],
+    grid: {
+      borderColor: '#FFFFFF',
+      padding: 0,
+    },
+  };
 
-    }];
-    const options = {
-        chart: {
-        type: 'bar',
-        width: '100%',
-        },
-        plotOptions: {
-        bar: {
-            borderRadius: 1,
-            horizontal: true,
-        }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        xaxis: {
-            categories: projectName,
-        },
-        colors:['#1C4E80', '#DBAE58', '#202020'],
-        grid: {
-            borderColor: '#FFFFFF',
-            padding: {
-                top: 0,
-                right: 0,
-                bottom: 0,
-                left: 0
-            },
-        }
-    };
-
-    return (
-        <div>
-        {loading ? (  
-          <div size="middle" style={{'height':'90vh', 'display':'flex', 'alignItems':'center', 'justifyContent':'center'}}>
-            <Spin size="large" />
-          </div>
-        ) : (<>
-        <div id="chart">
-            <ReactApexCharts options={options} series={series} type="bar" height={'240vh'} />
-        </div></>)}
+  return (
+    <div>
+      {loading ? ( // When data is still loading, show a spinner
+        <div
+          style={{
+            height: '90vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Spin size="large" />
         </div>
-    );
+      ) : ( // Otherwise, show the chart using ApexCharts
+        <div id="chart">
+          <ReactApexCharts options={options} series={series} type="bar" height={'240vh'} />
+        </div>
+      )}
+    </div>
+  );
 }
