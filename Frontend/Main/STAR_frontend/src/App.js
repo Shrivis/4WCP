f2c0c5f636aa2e63dc1c62778e26aaafa5685991
Vansh Aggarwal <vansh.aggarwal2@incedoinc.com>
2023-04-03 21:48:14 +0530

    comments added

--- Frontend/Main/STAR_frontend/src/App.js
+++ Frontend/Main/STAR_frontend/src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router,Routes,Route,Link, BrowserRouter} from 'react-router-dom';
-import Login from './Components/Login/login';
-import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
-import RouteLogin from './Components/PrivateRoute/RouteLogin';
-import Nav from './Nav';
-import React from 'react';
+// Importing necessary modules and components
+import './App.css'; // Stylesheet for App component 
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter} from 'react-router-dom'; // React routing components 
+import Login from './Components/Login/login'; // Login component 
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute'; // PrivateRoute component 
+import RouteLogin from './Components/PrivateRoute/RouteLogin'; // RouteLogin component 
+import Nav from './Nav'; // Navigation menu component 
+import React from 'react'; // React library 
 
 function App() {
   return (
-    <BrowserRouter basename={'/star'}>
+    <BrowserRouter basename={'/star'}> {/*Setting the base URL for all routes*/} 
       <Routes >
-        <Route exact path="/" element={<RouteLogin/>}/>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/home" element={<PrivateRoute/>}>
-        <Route path="" element={<Nav/>}/>
+        <Route exact path="/" element={<RouteLogin/>}/> {/*Route for login page*/} 
+        <Route exact path="/login" element={<Login/>}/> {/*Route for the actual login page*/} 
+        <Route exact path="/home" element={<PrivateRoute/>}> {/*Private route for home page*/}
+        <Route path="" element={<Nav/>}/> {/*Nested route for navigation menu*/} 
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
-
+export default App; // Exporting App component
